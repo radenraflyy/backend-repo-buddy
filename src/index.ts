ab9790fb677f8965fefccf2f95ad31800ccc0fa4
Raden Rafly Pradana Kusumah <radenraflypradanakusumah@gmail.com>
2025-04-28 15:46:26 +0700

    add configuration listen project

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
-import express from "express"
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
+import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import http from 'http';
+import cors from 'cors';
 
-var router = require("./routes/index")
+var router = require('./routes/index');
 
-var app = express()
+const PORT = process.env.PORT || 3001;
 
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+const app: Express = express();
+const server = http.createServer(app);
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FE_ORIGIN?.split(','),
+  }),
+);
 
-app.use("/", router)
-app.use("/users", router)
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-module.exports = app
+app.use('/v1', router);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+server.on('error', (error) => {
+  console.error('Server error:', error);
+});
